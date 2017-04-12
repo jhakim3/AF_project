289d3b86ea24914fc01b21685d128f93f1069ed6
Joe Hakim <jhakim3@jhu.edu>
2017-04-12 10:58:58 -0400

    fixed typo in plot_stuff

--- plot_classifier_comparison.py
+++ plot_classifier_comparison.py
@@ -47,6 +47,8 @@ from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.metrics import roc_curve, auc
 from sklearn.feature_selection import chi2, f_classif, mutual_info_classif
 from sklearn.feature_selection import SelectKBest
+from sklearn.metrics import precision_recall_curve
+from scipy.stats import sem
 
 h = .02  # step size in the mesh
 
@@ -83,62 +85,104 @@ datasets = [dynamic_dataset]
 plot_x=0
 plot_y=0
 f,axarr=plt.subplots(2,5)
-for ds_cnt, ds in enumerate(datasets):
-    # preprocess dataset, split into training and test part
-    X, y = ds
-    X = StandardScaler().fit_transform(X)
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, test_size=.5, random_state=42)
 
-    # iterate over classifiers
-    for name, clf in zip(names, classifiers):
-        clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
-        print('{0} accuracy: {1}'.format(name,score))
+plt.figure()
 
-        if(name == "Linear SVM"):
-            proba=clf.decision_function(X_test)
-        elif(name == "RBF SVM"):
-            proba=clf.decision_function(X_test)
-        else:
-            proba=clf.predict_proba(X_test)
-            proba=np.transpose(proba[:,1])
-            
+param_iterer=0
 
-        print(proba)
+aucs_rep=np.ndarray(shape=(10,3))
+for param_iter in (0.3, 0.4, 0.5):
+    plot_x=0
+    plot_y=0
+    for ds_cnt, ds in enumerate(datasets):
+        # preprocess dataset, split into training and test part
+        X, y = ds
+        X = StandardScaler().fit_transform(X)
+        X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size=param_iter, random_state=42)
 
-        fpr=dict()
-        tpr=dict()
-        roc_auc=dict()
-        for roc_ind in range(len(y)):
-            fpr[roc_ind], tpr[roc_ind], _ = roc_curve(y_test,proba)
-            roc_auc[roc_ind] = auc(fpr[roc_ind], tpr[roc_ind])
+        # iterate over classifiers
+        for name, clf in zip(names, classifiers):
+            clf.fit(X_train, y_train)
+            score = clf.score(X_test, y_test)
+            #print('{0} accuracy: {1}'.format(name,score))
+
+            if(name == "Linear SVM"):
+                proba=clf.decision_function(X_test)
+            elif(name == "RBF SVM"):
+                proba=clf.decision_function(X_test)
+            else:
+                proba=clf.predict_proba(X_test)
+                proba=np.transpose(proba[:,1])
+
+            fpr=dict()
+            tpr=dict()
+            roc_auc=dict()
+
+            fpr["micro"],tpr["micro"],_=roc_curve(y_test,proba)
+            roc_auc["micro"]=auc(fpr["micro"],tpr["micro"])
+
+            aucs_rep[plot_x,param_iterer]=roc_auc["micro"]
+                
+
+
+            #precision = dict()
+            #recall = dict()
+            #average_precision = dict()
+            #for pr_ind in range(len(y)):
+            #    precision[pr_ind], recall[pr_ind], _ = precision_recall_curve(y_test,proba)
 
         
-        plot_x_coord=0 if plot_x >= 5 else 1
-        _, plot_y_coord=divmod(plot_y,5)
-        axarr[plot_x_coord,plot_y_coord].plot(fpr[2],tpr[2],lw=2,label='AUC = %0.2f' % roc_auc[2])
-        axarr[plot_x_coord,plot_y_coord].legend(loc="lower right",markerscale=0)
+            #plot_x_coord=0 if plot_x >= 5 else 1
+            #_, plot_y_coord=divmod(plot_y,5)
+            #axarr[plot_x_coord,plot_y_coord].plot(fpr["micro"],tpr["micro"],lw=2,label='AUC = %0.2f' % roc_auc["micro"])
+            #axarr[plot_x_coord,plot_y_coord].legend(loc="lower right",markerscale=0)
 
-        axarr[plot_x_coord,plot_y_coord].plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+            #axarr[plot_x_coord,plot_y_coord].plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
         
-        #axarr[plot_x_coord,plot_y_coord].set_xlim([0.0, 1.0])
-        #axarr[plot_x_coord,plot_y_coord].set_ylim([0.0, 1.05])
-        if(plot_x_coord==1):
-            axarr[plot_x_coord,plot_y_coord].set_xlabel('False Positive Rate')
-        if(plot_y_coord==0):
-            axarr[plot_x_coord,plot_y_coord].set_ylabel('True Positive Rate')
-        axarr[plot_x_coord,plot_y_coord].set_title('%s' % name)
-        #axarr[plot_x_coord,plot_y_coord].annotate('AUC = %0.2f' % roc_auc[2], xy=(-12, -12), xycoords='axes points',
-        #    size=14, ha='right', va='top',
-        #    bbox=dict(boxstyle='round', fc='w'))
+            ##axarr[plot_x_coord,plot_y_coord].set_xlim([0.0, 1.0])
+            ##axarr[plot_x_coord,plot_y_coord].set_ylim([0.0, 1.05])
+            #if(plot_x_coord==1):
+            #    axarr[plot_x_coord,plot_y_coord].set_xlabel('False Positive Rate')
+            #if(plot_y_coord==0):
+            #    axarr[plot_x_coord,plot_y_coord].set_ylabel('True Positive Rate')
+            #axarr[plot_x_coord,plot_y_coord].set_title('%s' % name)
+            ##axarr[plot_x_coord,plot_y_coord].annotate('AUC = %0.2f' % roc_auc[2], xy=(-12, -12), xycoords='axes points',
+            ##    size=14, ha='right', va='top',
+            ##    bbox=dict(boxstyle='round', fc='w'))
 
-        plt.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
-        plt.setp([a.get_yticklabels() for a in axarr[:, 1]], visible=False)
-        plt.setp([a.get_yticklabels() for a in axarr[:, 2]], visible=False)
-        plt.setp([a.get_yticklabels() for a in axarr[:, 3]], visible=False)
-        plt.setp([a.get_yticklabels() for a in axarr[:, 4]], visible=False)
+            #plt.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
+            #plt.setp([a.get_yticklabels() for a in axarr[:, 1]], visible=False)
+            #plt.setp([a.get_yticklabels() for a in axarr[:, 2]], visible=False)
+            #plt.setp([a.get_yticklabels() for a in axarr[:, 3]], visible=False)
+            #plt.setp([a.get_yticklabels() for a in axarr[:, 4]], visible=False)
 
-        plot_y+=1
-        plot_x+=1 
+            plot_y+=1
+            plot_x+=1 
+
+        
+            #plot_x_coord=0 if plot_x >= 5 else 1
+            #_, plot_y_coord=divmod(plot_y,5)
+            #axarr[plot_x_coord,plot_y_coord].plot(precision[2],recall[2],lw=2)
+        
+
+            #if(plot_x_coord==1):
+            #    axarr[plot_x_coord,plot_y_coord].set_xlabel('Recall')
+            #if(plot_y_coord==0):
+            #    axarr[plot_x_coord,plot_y_coord].set_ylabel('Precision')
+            #axarr[plot_x_coord,plot_y_coord].set_title('%s' % name)
+            ##axarr[plot_x_coord,plot_y_coord].annotate('AUC = %0.2f' % roc_auc[2], xy=(-12, -12), xycoords='axes points',
+            ##    size=14, ha='right', va='top',
+            ##    bbox=dict(boxstyle='round', fc='w'))
+
+            #plt.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
+            #plt.setp([a.get_yticklabels() for a in axarr[:, 1]], visible=False)
+            #plt.setp([a.get_yticklabels() for a in axarr[:, 2]], visible=False)
+            #plt.setp([a.get_yticklabels() for a in axarr[:, 3]], visible=False)
+            #plt.setp([a.get_yticklabels() for a in axarr[:, 4]], visible=False)
+
+            #plot_y+=1
+            #plot_x+=1 
+    param_iterer+=1
+print(aucs_rep)
 plt.show()
--- plot_stuff.m
+++ plot_stuff.m
@@ -1,5 +1,7 @@
+%run after random-forest-discriminator.m
+
 close all
-labels={'Total Fibrosis Burden','ML Region Percentage','ML in RD Area','ML Proportion in RD Area','ML in Non RD Area','Fibrosis density','Fibrosis entropy','Lesion perim.','Lesion area','Lesion perim over area','min-EFA','Euclid','Dijkstra','Borderiness, LA','Borderiness, RA','# PSs','#PSs No Duplicates','# PS clusters (e = .05)','# PS clusters (e = .1)','Lesion AR','Lesion FF','Lesion R','Lesion Con','Dijstra to };
+labels={'Total Fibrosis Burden','ML Region Percentage','ML in RD Area','ML Proportion in RD Area','ML in Non RD Area','Fibrosis density','Fibrosis entropy','Lesion perim.','Lesion area','Lesion perim over area','min-EFA','Euclid','Dijkstra','Borderiness, LA','Borderiness, RA','# PSs','#PSs No Duplicates','# PS clusters (e = .05)','# PS clusters (e = .1)','Lesion AR','Lesion FF','Lesion R','Lesion Con','Dijstra to LIPV','Dijstra to LSPV','Dijkstra to RIPV','Dijstra to RSPV','Dijkstra to IVC','Dijstra to SVC','Dijstra to MV','Dijstra to TV','Euclid to LIPV','Euclid to LSPV','Euclid to RIPV','Euclid to RSPV','Euclid to IVC','Euclid to SVC','Euclid to MV','Euclid to TV','Min Dijkstra to PVs','Min Euclid to PVs'};
 for i=1:size(M,2)
     figure
 
@@ -30,38 +32,38 @@ end
 sitesToOmit=[1,2,3,5,9,18,21,23,25,27,56];
 sitesToOmitAgain=[13,36,44,55,57,61];
 
-
-%misc stuff
-dDists=dDist{3};
-dDists=reshape(dDists,73,8);
-dDists=dDists(setdiff(1:length(dDists),sitesToOmit),:);
-dDists=dDists(setdiff(1:length(dDists),sitesToOmitAgain),:);
-%dlmwrite('C:/Workspace/dDists.txt',dDists,'delimiter','\t')
-dlmwrite('C:/Workspace/dDists.txt',min(dDists(:,1:4),[],2),'delimiter','\t')
-
-eDists=eDist{3};
-eDists=reshape(eDists,73,8);
-eDists=eDists(setdiff(1:length(eDists),sitesToOmit),:);
-eDists=eDists(setdiff(1:length(eDists),sitesToOmitAgain),:);
-%dlmwrite('C:/Workspace/eDists.txt',eDists,'delimiter','\t')
-dlmwrite('C:/Workspace/eDists.txt',min(eDists(:,1:4),[],2),'delimiter','\t')
-
-
-
-%mutual info stuff
-
-MIs=zeros(5,5);
-M_temp2=M_temp;
-M_temp2(:,10)=
-M_zscore=zscore(M,0,1);
-indices=[10,20,21,22,23];
-for i=1:5
-    for j=1:5
-        MIs(i,j)=mutualinfo(M_zscore(:,indices(i)),M_zscore(:,indices(j)));
-    end
-end
-
-
+% 
+% %misc stuff
+% dDists=dDist{3};
+% dDists=reshape(dDists,73,8);
+% dDists=dDists(setdiff(1:length(dDists),sitesToOmit),:);
+% dDists=dDists(setdiff(1:length(dDists),sitesToOmitAgain),:);
+% %dlmwrite('C:/Workspace/dDists.txt',dDists,'delimiter','\t')
+% dlmwrite('C:/Workspace/dDists.txt',min(dDists(:,1:4),[],2),'delimiter','\t')
+% 
+% eDists=eDist{3};
+% eDists=reshape(eDists,73,8);
+% eDists=eDists(setdiff(1:length(eDists),sitesToOmit),:);
+% eDists=eDists(setdiff(1:length(eDists),sitesToOmitAgain),:);
+% %dlmwrite('C:/Workspace/eDists.txt',eDists,'delimiter','\t')
+% dlmwrite('C:/Workspace/eDists.txt',min(eDists(:,1:4),[],2),'delimiter','\t')
+% 
+% 
+% 
+% %mutual info stuff
+% 
+% MIs=zeros(5,5);
+% M_temp2=M_temp;
+% 
+% M_zscore=zscore(M,0,1);
+% indices=[10,20,21,22,23];
+% for i=1:5
+%     for j=1:5
+%         MIs(i,j)=mutualinfo(M_zscore(:,indices(i)),M_zscore(:,indices(j)));
+%     end
+% end
+% 
+% 
 
 
 
